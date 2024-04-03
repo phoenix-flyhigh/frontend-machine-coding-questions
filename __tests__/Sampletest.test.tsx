8f1dcdbb92ebc5f670788f18adaeb1e7b4e37933
phoenix-flyhigh <j.akshaya07@gmail.com>
2024-04-03 12:20:42 +0530

    Add products table with sorting

--- __tests__/Sampletest.test.tsx
+++ __tests__/Sampletest.test.tsx
@@ -1,11 +1,59 @@
-import {render} from '@testing-library/react';
-import React from 'react';
-import App from '../src/App';
+import {
+  render,
+  screen,
+  waitForElementToBeRemoved,
+} from "@testing-library/react";
+import React from "react";
+import App, { Product } from "../src/App";
 import axios from "axios";
 
-test('jest running', () => {
-  jest.spyOn(axios, "get").mockResolvedValue([]);
-  
-  render(<App />);
-  expect(true).toBe(true)
+describe("App tests", () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  test("should render products table ", async () => {
+    const mockProducts: Product[] = [
+      {
+        id: 1,
+        title: "shirt",
+        description: "new cottom t-shirt",
+        price: 799,
+      },
+      {
+        id: 2,
+        title: "pants",
+        description: "denim jeans",
+        price: 997,
+      },
+    ];
+    jest.spyOn(axios, "get").mockResolvedValue({ data: mockProducts });
+
+    render(<App />);
+    await waitForElementToBeRemoved(() =>
+      screen.queryByText("Loading products")
+    );
+    const rows: HTMLElement[] = screen.getAllByTestId("tid-table-row");
+
+    expect(rows.length).toBe(2);
+  });
+
+  test("Should render loading msg until products are loaded", () => {
+    jest.spyOn(axios, "get").mockResolvedValue({ data: [] });
+
+    render(<App />);
+
+    expect(screen.getByText("Loading products")).toBeInTheDocument();
+  });
+
+  test("Should render error msg when products api request fails", async () => {
+    jest.spyOn(axios, "get").mockRejectedValue("internal error");
+
+    render(<App />);
+    await waitForElementToBeRemoved(() =>
+      screen.queryByText("Loading products")
+    );
+
+    expect(screen.getByText("Error in fetching products")).toBeInTheDocument();
+  });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,93 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
+import axios from "axios";
+
+enum SortOrder {
+  ASC = "asc",
+  DESC = "desc",
+}
+
+export interface Product {
+  id: number;
+  title: string;
+  description: string;
+  price: number;
+}
+
+enum FetchStatus {
+  INITIAL = "INITIAL",
+  LOADING = "LOADING",
+  SUCCESS = "SUCCESS",
+  ERROR = "ERROR",
+}
 
 function App() {
+  const [currentSortOrder, setCurrentSortOrder] = useState<SortOrder>(
+    SortOrder.ASC
+  );
+  const [products, setProducts] = useState<Product[]>([]);
+  const [status, setStatus] = useState<FetchStatus>(FetchStatus.INITIAL);
+
+  useEffect(() => {
+    setStatus(FetchStatus.LOADING);
+    axios
+      .get(`https://fakestoreapi.com/products?sort=${currentSortOrder}`)
+      .then(async (response) => {
+        const data: Product[] = await response.data;
+        setProducts(data);
+        console.log(data);
+        
+        setStatus(FetchStatus.SUCCESS);
+      })
+      .catch(() => {
+        console.log("error occured in api fetch");
+        setStatus(FetchStatus.ERROR);
+      });
+  }, [currentSortOrder]);
+
   return (
-    <div className="bg-black h-screen w-full flex justify-center items-center">
-      <h1 className="text-3xl text-white">Vite + React + TS + Testing Template</h1>
+    <div className="bg-gray-200 p-4 flex flex-col gap-4 items-center">
+      <div className="pr-2 bg-white w-fit rounded-lg">
+        <select
+          data-testid="tid-select-box"
+          value={currentSortOrder}
+          onChange={(e) => setCurrentSortOrder(e.target.value as SortOrder)}
+          className="border-none bg-none px-4 py-2 "
+        >
+          <option value={SortOrder.ASC}>Ascending</option>
+          <option value={SortOrder.DESC}>Descending</option>
+        </select>
+      </div>
+      {status === FetchStatus.SUCCESS && (
+        <table className="flex flex-col border-slate-500 border-2 border-collapse">
+          <thead className="grid grid-cols-4">
+            <th className="text-center border-2 border-slate-500">ID</th>
+            <th className="border-2 border-slate-500"> Title</th>
+            <th className="border-2 border-slate-500"> Description</th>
+            <th className="border-2 border-slate-500"> Price</th>
+          </thead>
+          <tbody>
+            {products.map(({ id, title, description, price }: Product) => (
+              <tr
+                className="grid grid-cols-4"
+                key={id}
+                data-testid="tid-table-row"
+              >
+                <td className="text-center border-2 border-slate-500">{id}</td>
+                <td className="flex  border-2 border-slate-500">{title}</td>
+                <td className="flex overflow-scroll border-2 border-slate-500">
+                  {description}
+                </td>
+                <td className="text-center border-2 border-slate-500">
+                  {price}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
+      {status === FetchStatus.LOADING && <div>Loading products</div>}
+      {status === FetchStatus.ERROR && <div>Error in fetching products</div>}
     </div>
   );
 }
