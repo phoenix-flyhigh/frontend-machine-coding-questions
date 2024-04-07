2b9bc6694bcfa7a85408c7f42941d1d0d35f9c3c
phoenix-flyhigh <j.akshaya07@gmail.com>
2024-04-07 09:24:12 +0530

    Add feedback wrapper

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,78 @@
-import React from "react";
+import { ReactNode, useState } from "react";
 import "./App.css";
+
+type Submithandler = (obj: { rating: number }) => void;
+interface FeedbackWrapperProps {
+  onSubmit: Submithandler;
+  children: ReactNode;
+}
+
+const RatingDescription = {
+  "1": "Unsatisfied",
+  "2": "Somewhat satisfied",
+  "3": "Satisfied",
+  "4": "Very satisfied",
+  "5": "Extremely satisfied",
+};
+
+const FeedbackWrapper = ({ onSubmit, children }: FeedbackWrapperProps) => {
+  const [clickCount, setClickCount] = useState(0);
+  const [rating, setRating] = useState("4");
+
+  return clickCount >= 3 ? (
+    <div className="flex flex-col justify-center items-center w-[90%] h-72 bg-orange-300">
+      <form
+        className="flex flex-col justify-center items-center gap-8"
+        onSubmit={(e) => {
+          e.preventDefault();
+          onSubmit({ rating: parseInt(rating) });
+          setClickCount(0);
+        }}
+      >
+        <div className="flex gap-8 w-80 items-center justify-start">
+          <input
+            type="range"
+            min={1}
+            max={5}
+            step={1}
+            value={rating}
+            onChange={(e) => setRating(e.target.value)}
+            className="cursor-pointer m-0"
+          />
+          <span>
+            {RatingDescription[rating as keyof typeof RatingDescription]}
+          </span>
+        </div>
+        <button className="cursor-pointer border border-black px-2 py-1 rounded-md bg-gray-100 ">
+          Submit
+        </button>
+      </form>
+    </div>
+  ) : (
+    <div
+      id="feedback-wrapper"
+      onClick={() => setClickCount((prev) => prev + 1)}
+    >
+      {children}
+    </div>
+  );
+};
 
 function App() {
   return (
-    <div className="bg-black h-screen w-full flex justify-center items-center">
-      <h1 className="text-3xl text-white">Vite + React + TS + Testing Template</h1>
+    <div className="h-screen w-full flex justify-center items-center">
+      <FeedbackWrapper
+        onSubmit={(obj: { rating: number }) => {
+          alert(`Rating from modal: ${obj.rating}`);
+        }}
+      >
+        {/* UI composed of three no-op buttons */}
+        <div style={{ display: "flex", marginBottom: 10 }}>
+          <button>A button</button>
+          <button>Another button</button>
+        </div>
+        <button style={{ padding: 20 }}>A bigger button</button>
+      </FeedbackWrapper>
     </div>
   );
 }
