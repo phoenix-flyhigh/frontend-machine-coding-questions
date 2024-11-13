6e71c6633014b17ab2cc1e9074a201520a84cee7
phoenix-flyhigh <j.akshaya07@gmail.com>
2024-11-13 12:26:04 +0530

    Update use callback instead of use effect to observe, add data incrementally

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,33 @@
-import { useEffect, useRef, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 
 function App() {
-  const data = [...Array(100)].map((_, i) => i + 1);
+  const [data, setData] = useState<number[]>([])
   const [page, setPage] = useState(1);
-  const sentinel = useRef<HTMLDivElement>(null);
 
-  useEffect(() => {
-    const observer = new IntersectionObserver((entries) => {
-      if (entries[0].isIntersecting) {
-        setPage((prev) => prev + 1);
-      }
-    });
-    const element = sentinel.current;
-    if (element) {
-      observer.observe(element);
+  const observer = useRef<IntersectionObserver>();
+  const sentinel = useCallback((node: HTMLDivElement) => {
+  if (observer.current) observer.current.disconnect();
+
+  observer.current = new IntersectionObserver((entries) => {
+    if (entries[0].isIntersecting) {
+      setPage((prev) => prev + 1);
     }
+  });
+  if (node) {
+    observer.current.observe(node);
+  }
+}, []);
 
-    return () => {
-      if (element) {
-        observer.unobserve(element);
-      }
-    };
-  }, []);
+useEffect(() => {
+  const baseNumber = (page - 1) * 10  // ex: start from 0 for page 1, start from 10 for page 2 ...
+  const newData = [...Array(10)].map((_, i) => baseNumber + i + 1) // Generate next 10 numbers 
+  setData(prev => [...prev, ...newData])
+}, [page] )
 
   return (
     <div className="bg-black text-white h-screen w-full flex justify-center items-center">
       <div className="w-64 border-white border-2 rounded-2xl h-80 overflow-scroll">
-        {data.slice(0, page * 10).map((x) => (
+        {data.map((x) => (
           <div className="px-4 py-2" key={x}>
             {x}
           </div>
